54eb4db1f2911ccaf8abd75c6b6e5ce4de2d49d5
Andy Schwartz <andy-git@schegg.org>
2012-09-21 17:24:49 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '154268ae31b5455a43fa9014d0c6eb6aaaf590cfc2cbfc821560329a3c3f723afe1e7b83c9a1e33f7cf1dacc0d8087b1b059bf674e72c707bf019aecd484a5b4'
